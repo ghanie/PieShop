250b74de64bbf8315856956778ea4144e97fbda5
Abdallah Abdulghany <abdallahabdulghany@gmail.com>
2020-12-11 22:27:18 +0200

    Creating Home Page

---
+++ PieShopWebsite/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PieShopWebsite.Services;
+using PieShopWebsite.ViewModels;
+
+namespace PieShopWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
